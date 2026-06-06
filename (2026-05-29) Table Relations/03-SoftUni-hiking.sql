280b74a30698626a8246cc43e5258cc15780e30f
Victor Marinov <195519767+vicmarinov@users.noreply.github.com>
2026-06-06 23:53:40 +0300

    Add solutions to the problems from 29-th May 2026

---
+++ (2026-05-29) Table Relations/03-SoftUni-hiking.sql
@@ -0,0 +1,7 @@
+SELECT
+    r.start_point,
+    r.end_point,
+    r.leader_id,
+    CONCAT_WS(' ', c.first_name, c.last_name) AS leader_name
+FROM routes AS r
+JOIN campers AS c ON r.leader_id = c.id;
